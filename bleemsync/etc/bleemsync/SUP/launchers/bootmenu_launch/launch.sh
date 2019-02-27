df5a4570ee4f1d0ec34a7d6f6cb9db503cea3a12
Ryan 'Swingflip' Hamlin <ryan@ryanhamlin.co.uk>
2019-02-27 18:08:06 +0000

    Added POC launchers for RA and BootMenu (BootMenu doesn't work it's WIP)

---
+++ bleemsync/etc/bleemsync/SUP/launchers/bootmenu_launch/launch.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+### LOAD CONFIGURATION ########################################################
+source "/var/volatile/bleemsync.cfg"
+
+### LOAD FUNCTION LIBRARIES ###################################################
+source "/media/bleemsync/etc/bleemsync/FUNC/0050_bleemsync.funcs"
+
+launch_BootMenu
